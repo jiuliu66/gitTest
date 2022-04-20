4308accb8c1e8f381c0472032b68f7ea79e1860f
chenzhe <1533229654@qq.com>
2022-04-20 12:32:58 +0800

    first commit

---
+++ src/main/java/TestGit.java
@@ -0,0 +1,8 @@
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+    }
+}
